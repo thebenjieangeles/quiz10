be987cf1aa123581991b1b20879c67ade66798ca
thebenjieangeles <zerofourtwentifive@gmail.com>
2023-07-10 05:16:15 +0700

    fixed JavaScript

--- script.js
+++ script.js
@@ -7,37 +7,37 @@ document.addEventListener("DOMContentLoaded", function () {
   var logo = document.getElementById("logo");
   var replay = document.getElementById("replay");
 
-  femaleImage.style.display = "block"; // Show the female image (fade-in animation)
-  headline1.style.display = "block"; // Show the first headline (hidden)
+  femaleImage.style.display = "block";
+  headline1.style.display = "block";
 
   setTimeout(function () {
-    headline1.style.animationName = "slide-in"; // Slide in the first headline from the left
+    headline1.style.animationName = "slide-in";
 
     setTimeout(function () {
-      femaleImage.style.animationName = "fade-out"; // Fade out the female image
+      femaleImage.style.animationName = "fade-out";
 
       setTimeout(function () {
-        headline1.style.animationName = "slide-to-top-right"; // Slide out the first headline to the right
+        headline1.style.animationName = "slide-to-top-right";
         headline2.style.animationName = "slide-to-left";
 
         setTimeout(function () {
-          headline1.style.display = "none"; // Hide the first headline
-          subheadline.style.display = "block"; // Show the second headline (hidden)
-          headline2.style.display = "block"; // Show the second headline (hidden)
+          headline1.style.display = "none";
+          subheadline.style.display = "block";
+          headline2.style.display = "block";
 
           setTimeout(function () {
-            LearnMorebtn.style.display = "block"; // Show the Learn More button (hidden)
+            LearnMorebtn.style.display = "block";
 
             setTimeout(function () {
-              logo.style.display = "block"; // Show the logo (hidden)
+              logo.style.display = "block";
 
               setTimeout(function () {
-                replay.style.display = "block"; // Show the logo (hidden)
+                replay.style.display = "block";
               }, 1000);
             }, 1000);
           }, 1000);
-        }, 1000); // Wait for fade-out and slide-out animations to complete before hiding the first headline
-      }, 1000); // Wait for the slide-in animation to complete before fading out the female image and sliding out the first headline
-    }, 1000); // Wait for the slide-in animation to complete before fading out the female image and sliding out the first headline
-  }, 1000); // Wait for 2 seconds after page load before starting the animations
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
 });
